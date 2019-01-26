4b51acdcae2853bdad562a09646b278d6f4b2f7b
nicks <nitikatimsy@gmail.com>
2019-01-26 00:01:42 -0800

    changes again

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
-//import {Button} from 'react-bootstrap';
-//import resume from'../Resume.pdf';
-//import {Button}
+
+
+
 
 class App extends Component {
   render() {
@@ -15,17 +15,13 @@ class App extends Component {
         <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
             <Navigation>
                <Link to="/resume">Resume</Link>
-
-
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
+               <Link to="/projects">Projects</Link>
                 <Link to="/contactme">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
             <Navigation>
               <Link to="/resume">Resume</Link>
-              <Link to="/aboutme">About Me</Link>
               <Link to="/projects">Projects</Link>
               <Link to="/contactme">Contact</Link>
             </Navigation>
--- src/components/main.js
+++ src/components/main.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import LandingPage from './landingpage';
-import AboutMe from './aboutme';
 import Contact from './contactme';
 import Projects from './projects';
 import Resume from './resume';
-//import Github from './github';
+
 
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
-    <Route path="/aboutme" component={AboutMe} />
     <Route path="/contactme" component={Contact} />
     <Route path="/projects" component={Projects} />
     <Route path="/resume" component={Resume} />
